675592226268d338631cba03a947a130af39be3c
Glitch (continuous-fuchsia-leader) <none>
2020-05-09 07:46:28 +0000

    🎪🐸 Checkpoint ./controllers/auth.controller.js:15532837/341 ./db.json:15532837/14

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -20,8 +20,9 @@ module.exports.postLogin = function(req, res){
 		return;
 	}	
   console.log('user.wrongLoginCount',user.wrongLoginCount);
-  if (user.wrongLoginCount <= 4) {
-    if (user.wrongLoginCount === 3) {
+  if (user.wrongLoginCount <= "3") {
+    if (user.wrongLoginCount === "2") {
+      console.log('Da send mail');
       const sgMail = require('@sendgrid/mail');
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
@@ -33,11 +34,11 @@ module.exports.postLogin = function(req, res){
       };
       sgMail.send(msg);  
     }
+    console.log('vai');
     var hashedPassword = bcrypt.hashSync(password, 10);
     if (!bcrypt.compareSync(user.password, hashedPassword)) {
       // count wrongLoginCount
       var countWrong = parseInt(user.wrongLoginCount) + 1;
-      console.log('countWrong',countWrong);
       db.get('users').find({id: user.id}).assign({wrongLoginCount: countWrong.toString()}).write();
       res.render('auth/login', {
         errors: ['Wrong password'],
