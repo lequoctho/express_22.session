2389cfdae39cd595686a5fae7b29e6ed92f1d7e4
Glitch (continuous-fuchsia-leader) <none>
2020-05-09 07:23:27 +0000

    🌮🎬 Checkpoint ./package.json:15532837/1630 ./controllers/auth.controller.js:15532837/1312 ./.gitignore:15532837/12

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,6 +21,18 @@ module.exports.postLogin = function(req, res){
 	}	
   console.log('user.wrongLoginCount',user.wrongLoginCount);
   if (user.wrongLoginCount <= 4) {
+    if (user.wrongLoginCount === 3) {
+      const sgMail = require('@sendgrid/mail');
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: test@example.com',
+        from: 'test@example.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+      sgMail.send(msg);  
+    }
     var hashedPassword = bcrypt.hashSync(password, 10);
     if (!bcrypt.compareSync(user.password, hashedPassword)) {
       // count wrongLoginCount
