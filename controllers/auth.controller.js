2f97af3be1b9a0c83b915e05f9c205fea860fd56
Glitch (continuous-fuchsia-leader) <none>
2020-05-09 10:09:25 +0000

    🌸😎 Checkpoint ./controllers/auth.controller.js:15532837/4005 ./db.json:15532837/2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,62 +1,74 @@
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
+const sgMail = require("@sendgrid/mail");
 
-var db = require('../db');
+var db = require("../db");
 
 module.exports.login = (req, res) => {
-  res.render('auth/login');
-}
+  res.render("auth/login");
+};
 
-module.exports.postLogin = function(req, res){
-	var email = req.body.email;
-	var password = req.body.password;
+module.exports.postLogin = function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
 
-	var user = db.get('users').find({ email: email }).value();
-	
-	if (!user) {
-		res.render('auth/login', {
-			errors: ['User does not exist.'],
-			values: req.body
-		});
-		return;
-	}	
-  console.log('user.wrongLoginCount',user.wrongLoginCount);
+  var user = db
+    .get("users")
+    .find({ email: email })
+    .value();
+
+  if (!user) {
+    res.render("auth/login", {
+      errors: ["User does not exist."],
+      values: req.body
+    });
+    return;
+  }
+  console.log("user.wrongLoginCount", user.wrongLoginCount);
   if (user.wrongLoginCount <= "3") {
     if (user.wrongLoginCount === "2") {
-      
-      const sgMail = require('@sendgrid/mail');
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
-        to: 'leequoctho@gmail.com',
-        from: 'testsendmaillequoctho@gmail.com',
-        subject: 'Test send mail',
-        text: 'Hello World',
-        html: '<strong>Hello World</strong>',
+        to: "leequoctho@gmail.com",
+        from: "testsendmaillequoctho@gmail.com",
+        subject: "Test send mail",
+        text: "Hello World",
+        html: "<strong>Hello World</strong>"
       };
-      sgMail.send(msg);  
-      console.log('sent mail');
+      sgMail.send(msg).then(
+        () => {},
+        error => {
+          console.error(error);
+          if (error.response) {
+            console.error(error.response.body);
+          }
+        }
+      );
+      console.log("sent mail");
     }
     var hashedPassword = bcrypt.hashSync(password, 10);
     if (!bcrypt.compareSync(user.password, hashedPassword)) {
       // count wrongLoginCount
       var countWrong = parseInt(user.wrongLoginCount) + 1;
-      db.get('users').find({id: user.id}).assign({wrongLoginCount: countWrong.toString()}).write();
-      res.render('auth/login', {
-        errors: ['Wrong password'],
+      db.get("users")
+        .find({ id: user.id })
+        .assign({ wrongLoginCount: countWrong.toString() })
+        .write();
+      res.render("auth/login", {
+        errors: ["Wrong password"],
         values: req.body
       });
       return;
-    } 
-  }
-  else {
-    res.render('auth/login', {
-        errors: ['Your account has been locked'],
-        values: req.body
-      });
-      return;
+    }
+  } else {
+    res.render("auth/login", {
+      errors: ["Your account has been locked"],
+      values: req.body
+    });
+    return;
   }
 
-	res.cookie('userId', user.id, {
+  res.cookie("userId", user.id, {
     signed: true
   });
-	res.redirect('/transactions');
+  res.redirect("/transactions");
 };
